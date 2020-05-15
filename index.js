00ed3aa20389545e16d8a4dad5951cd7c94e4fb8
neven <nevendrean@yahoo.fr>
2020-05-16 00:31:48 +0200

    URLSearchPArams & serialization & sintrigiy if needed FormData

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
-const display = async (mykey, myvalue) => {
+const uriu = "https://reqres.in/api/users/";
+const userForm = document.querySelector("form");
+
+const display = (mykey, myvalue) => {
   return document
     .querySelector("#resu1")
     .insertAdjacentHTML("beforeend", `<p>${mykey}: &nbsp ${myvalue} </p> <br>`);
 };
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const userForm = document.querySelector("form");
-  const myForm = new FormData(userForm);
+const insert = (text) => {
+  return document
+    .querySelector("#resu1")
+    .insertAdjacentHTML("beforeend", `<p> ${text}</p>`);
+};
 
-  const uriu = "https://reqres.in/api/users/";
-
-  // iterate on a FormData with FormData.entries and not Object.entries(FormData)
-  const show = async () => {
-    for (let [k, v] of myForm.entries()) {
-      display(k, v);
-    }
-  };
-
-  try {
-    const response = await fetch(uriu, { method: "POST", body: myForm });
-    const result = await response.json();
-    display("Post Fetch new ID :", result.id)
-      .then(show())
-      .then(document.querySelector("form").reset());
-  } catch (err) {
-    console.log(err);
+// iterate on a FormData using FormData.entries and not using  Object.entries(FormData)
+const serializeForm = (formData) => {
+  let obj = {};
+  let params = new URLSearchParams();
+  let payload = "";
+  for (let [k, v] of formData.entries()) {
+    obj[k] = v;
+    params.append(k, v);
   }
-});
+  return {
+    obj: obj,
+    queryString: params.toString(),
+    payload: JSON.stringify(obj),
+  };
+};
+
+async function getData(tag) {
+  document.querySelector(tag).addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+      const myform = new FormData(e.target);
+      const response = await fetch(uriu, {
+        method: "POST",
+        body: new FormData(e.target),
+        // body: serializeForm(myform).payload,
+      }); // new FormData(document.querySelector('form'))
+      if (!response.ok) {
+        return Promise.reject(response);
+      }
+      const data = await response.json();
+      insert(
+        "We normally pass the <code>new FormData(e.target)</code> to the <em>body</em> of the <em>fetch</em> request."
+      );
+      display("Your new ID is: ", data.id);
+      insert(
+        "The data in the form can be transformed into a query string for GET by URLSearchParams:"
+      );
+      insert(`<p> ${serializeForm(myform).queryString}</p>`);
+      insert(
+        "For a POST request, we pass also may to pass the FormData by serializing it into an object and stringify this json"
+      );
+      insert(`<p> ${serializeForm(myform).payload}</p>`);
+    } catch (error) {
+      console.warn(error);
+    }
+  });
+}
+
+getData("form").then(userForm.reset());
